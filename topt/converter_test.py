0c54d2987dc45bcbaa6de53e02a5d78221214916
Michael A. Perlin <mika.perlin@gmail.com>
2024-05-04 10:21:52 -0500

    add unit tests for converters

---
+++ topt/converter_test.py
@@ -0,0 +1,19 @@
+"""Unit tests for converter.py"""
+
+import jax
+
+import topt
+
+
+def test_conversions(dim: int = 5) -> None:
+    """Test conversions with random numbers."""
+    key = jax.random.PRNGKey(0)
+    vec = jax.random.normal(key, (dim,), dtype=complex)
+    mat = jax.random.normal(key, (dim, dim), dtype=complex)
+
+    vec_real = topt.converter.complex_to_real_vec(vec)
+    mat_real = topt.converter.complex_to_real_mat(mat)
+
+    assert jax.numpy.array_equal(vec, topt.converter.real_to_complex_vec(vec_real))
+    assert jax.numpy.array_equal(mat, topt.converter.real_to_complex_mat(mat_real))
+    assert jax.numpy.allclose(mat @ vec, topt.converter.real_to_complex_vec(mat_real @ vec_real))
